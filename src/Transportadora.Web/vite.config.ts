ebf3171c62fef2dc7db74bec6710932bdedfa1f2
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2025-12-14 17:04:14 -0300

    ...

--- src/Transportadora.Web/vite.config.ts
+++ src/Transportadora.Web/vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
